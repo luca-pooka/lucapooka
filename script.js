67f3b2441cb864cb36ae0d778bdd84dc8e38aa53
LucaPooka <lucasalvatore1202@gmail.com>
2025-09-24 20:55:06 -0400

    Made "New" tag more prominent

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apps = [
     {name: "Pong", tags: ["Unity", "Game", "Multiplayer", "Co-op", "Versus", "The Best"], link: "pong/", info: "Go against another player in the classic game of hitting a ball back and forth!"},
     {name: "Life Simulator", tags: ["Game", "Single Player", "The Best", "Mobile-friendly"], link: "lifesim/", info: "Create people you know in real life, solve their problems, and care for them."},
     {name: "Puzzle Platform", tags: ["Unity", "Game", "Single Player", "Beta", "New"], link: "puzzleplatform/", info: "Progress through levels in this 2D platformer puzzle game."},
-    {name: "Snake", tags: ["Game", "Single Player", "New"], link: "snake/", info: "Move around collecting fruit to grow and get a high score in this classic game."},
+    {name: "Snake", tags: ["Game", "Single Player"], link: "snake/", info: "Move around collecting fruit to grow and get a high score in this classic game."},
     {name: "Rolling", tags: ["Game", "Single Player", "Mobile-friendly"], link: "rolling/", info: "Roll and roll and try to get something good."},
     {name: "Blackjack", tags: ["Game", "Single Player", "Mobile-friendly"], link: "blackjack/", info: "Bet your money and try to make it big without going over 21."},
     {name: "Person Sort", tags: ["Game", "Single Player", "Mobile-friendly"], link: "personsort/", info: "Sort different colored squares into their correct houses."},
@@ -14,7 +14,7 @@ const apps = [
     {name: "Chess", tags: ["Game", "Multiplayer", "Versus", "Mobile-friendly", "Beta"], link: "chess/", info: "Advance your pieces and try to checkmate (or capture in this version) the opponent's king."},
     {name: "Connect Four", tags: ["Game", "Multiplayer", "Versus", "Mobile-friendly"], link: "connectfour/", info: "Try to get four of your color in a row before your opponent does."},
     {name: "Calculator", tags: ["Utility", "Single Player"], link: "calculator/", info: "It's a calculator, you can do your math homework or something."},
-    {name: "To-Do List", tags: ["Utility", "Single Player", "Mobile-friendly", "Beta", "New"], link: "todo/", info: "Put your tasks in and check them off when completed."},
+    {name: "To-Do List", tags: ["Utility", "Single Player", "Mobile-friendly", "Beta"], link: "todo/", info: "Put your tasks in and check them off when completed."},
     {name: "Random Number Gen", tags: ["Utility", "Single Player", "Mobile-friendly"], link: "rng/", info: "Get a random number from the range you pick."},
     {name: "Find your exact age", tags: ["Utility", "Single Player", "Mobile-friendly"], link: "exactage/", info: "Put in your date and time of birth, and you will be told exactly how old you are."},
     //{name: "Stair Stepper", tags: ["Game", "Single Player", "Multiplayer", "Versus", "Beta"], link: "stairstep/", info: "Don't choose the same thing as your opponents, or else you won't climb up. (Doesn't work)"}
@@ -59,7 +59,6 @@ function createApps() {
             conditions.push(input.value);
         }
     });
-    console.log(conditions);
     checkedApps = apps.filter(app => {
         return conditions.every(item => app.tags.includes(item));
     });
@@ -76,6 +75,10 @@ function createApps() {
         square.appendChild(title);
         const tags = document.createElement("h5");
         tags.innerHTML = app.tags.join(", ");
+        const index = tags.innerHTML.search("New");
+        if (index != -1) {
+            tags.innerHTML = tags.innerHTML.slice(0, index) + "<span style='color: gold'>" + tags.innerHTML.slice(index) + "</span>";
+        }
         square.appendChild(tags);
         const description = document.createElement("p");
         description.innerHTML = app.info;
