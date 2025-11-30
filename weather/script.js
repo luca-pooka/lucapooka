544fff50ed6c47579757a4dc91a5d47251792b4f
LucaPooka <lucasalvatore1202@gmail.com>
2025-11-30 11:52:01 -0500

    Updated Weather to ver 1.1, making it be able to select certain days, reset to today, look better, and added credits to the APIs

--- weather/script.js
+++ weather/script.js
@@ -1,4 +1,5 @@
 const city = document.getElementById("city");
+const weather = document.getElementById("weather");
 const forecast = document.getElementById("forecast");
 // const forecastDate = forecast.querySelector(".date");
 const forecastHighLow = forecast.querySelector("#high-low");
@@ -7,13 +8,33 @@ const forecastNighttime = forecast.querySelector("#nighttime");
 let periodIndex = 0;
 city.value = JSON.parse(localStorage.getItem("weather-city"));
 
+const date = new Date();
+const daysHolder = document.getElementById("days-holder");
+const daysList = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+for (let index = 0; index < 7; index++) {
+    const dayNumber = (date.getDay() + index) % 7;
+    const button = document.createElement("button");
+    button.className = `day day${index}`;
+    if (index === 0) button.classList.add("selected");
+    button.innerHTML = daysList[dayNumber];
+    button.onclick = () => daySet(dayNumber * 2);
+    daysHolder.appendChild(button);
+    console.log(daysList[dayNumber]);
+}
+
 async function getWeather() {
+    weather.style.display = "block";
     forecastHighLow.innerHTML = "Loading...";
+    for (let index = 0; index < 7; index++) {
+        const day = daysHolder.querySelector(`.day${index}`);
+        day.classList.remove("selected");
+        if (Math.floor(periodIndex / 2) === index) day.classList.add("selected");
+    }
     localStorage.setItem("weather-city", JSON.stringify(city.value));
     try {
         if (periodIndex < 0) periodIndex = 0;
         const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(city.value)}`;
-        const cityResponse = await fetch(url, { headers: { "User-Agent": "LucaPooka Weather App (Project)" } });
+        const cityResponse = await fetch(url, { headers: { "User-Agent": "LucaPooka Weather Project" } });
         const cityData = await cityResponse.json();
         console.log(cityData);
         if (cityData.length === 0) throw new Error("City not found.");
@@ -34,13 +55,13 @@ async function getWeather() {
         const forecastToday = forecastPeriods[periodIndex];
         const forecastTonight = forecastPeriods[periodIndex + 1];
         console.log(forecastTonight.name);
-        forecastHighLow.innerHTML = `High: ${forecastToday.temperature}° | Low: ${forecastTonight.temperature}°`;
+        forecastHighLow.innerHTML = `High: ${forecastToday.temperature}°${forecastToday.temperatureUnit} | Low: ${forecastTonight.temperature}°${forecastToday.temperatureUnit}`;
         function showForecast(timeElement, forecast) {
             timeElement.querySelector(".time").innerHTML = `${forecast.name}:`;
-            timeElement.querySelector(".detailed").innerHTML = `Summary:<br>${forecast.detailedForecast}`;
             timeElement.querySelector(".forecast").innerHTML = `${forecast.temperature}° — ${forecast.shortForecast}`;
             timeElement.querySelector(".precipitation").innerHTML = `Chance of precipitation: ${forecast.probabilityOfPrecipitation.value}%`;
             timeElement.querySelector(".wind").innerHTML = `Wind Speed: ${forecast.windSpeed} — ${forecast.windDirection}`;
+            timeElement.querySelector(".detailed").innerHTML = `Summary:<br>${forecast.detailedForecast}`;
         }
         showForecast(forecastDaytime, forecastToday);
         if (forecastToday.isDaytime) { // if that forecast was for the daytime, show the next forecast for the nighttime
@@ -56,5 +77,9 @@ async function getWeather() {
 }
 function dayChange(change) {
     periodIndex += change;
+    getWeather();
+}
+function daySet(set) {
+    periodIndex = set;
     getWeather();
 }
